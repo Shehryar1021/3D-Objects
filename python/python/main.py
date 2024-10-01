59802bc9ae054b4b1c597766537c8c8bdc5f6df5
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-01 15:29:03 +0500

    INITIAL COMMIT

---
+++ python/python/main.py
@@ -0,0 +1,9 @@
+
+print(x)
+print(y)
+print("HAPPY BIRTHDAY")
+print(codingal)
+
+name = input("enter your name:")
+print("\HELLO", name, "\WELCOME TO CODINGAL")
+
---
+++ welcome to world of coding/WELCOME TO CODING/main.py
@@ -0,0 +1,10 @@
+
+x = 5 
+y = "John"
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal)
+
+name = input("enter your name:")
+print("\nHELLO", name, "\nWELCOME TO CODINGAL")
